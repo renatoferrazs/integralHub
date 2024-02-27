fdb07dab0ea9e8af01873473ac292188603a89c7
RenatoFerraz <renatoferrazs@hotmail.com>
2024-02-27 10:26:24 -0300

    testes

---
+++ src/main/java/br/com/integralHub/IntegralHubApplication.java
@@ -0,0 +1,13 @@
+package br.com.integralHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegralHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegralHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/integralHub/IntegralHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.integralHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegralHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
